02fb663576612ca62fd102271b26b7e81dd42336
unwind <ysh038@naver.com>
2022-03-29 17:31:39 +0900

    JS : Home transparent

--- main.js
+++ main.js
@@ -51,6 +51,13 @@ homeContactBtn.addEventListener("click", () => {
     scrollIntoView("#contact");
 });
 
+//Make some slowly fade to transparent as the window scrolls down
+const home = document.querySelector(".home__container");
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener("scroll", () => {
+    home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
 function scrollIntoView(selector) {
     const scrollTo = document.querySelector(selector);
     scrollTo.scrollIntoView({ behavior: "smooth" });
