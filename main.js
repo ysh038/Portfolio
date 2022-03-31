c1a70ad65fb964e63537e43da5d73618b4a96f6e
unwind <ysh038@naver.com>
2022-03-31 15:55:41 +0900

    JS : remove active from nav

--- main.js
+++ main.js
@@ -84,6 +84,15 @@ workBtnContainer.addEventListener("click", (e) => {
     if (filter == null) {
         return;
     }
+
+    //Remove selection from the previous item and select the new one
+    const active = document.querySelector(".category__btn.selected");
+    active.classList.remove("selected");
+    console.log(e.target.nodeName);
+    const target =
+        e.target.nodeName === "BUTTON" ? e.target : e.target.parentNode;
+    target.classList.add("selected");
+
     projectContainer.classList.add("anim-out");
 
     setTimeout(() => {
@@ -97,7 +106,7 @@ workBtnContainer.addEventListener("click", (e) => {
         projectContainer.classList.remove("anim-out");
     }, 300);
 
-    /*
+    /* 밑 for문과 forEach문이 같은 기능을 함
     let project;
     for(let i=0; i<projects.length; i++){
         project = projects[i];
