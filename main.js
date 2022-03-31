c61978246afb9d0442515235b9ffa27bc07018c2
unwind <ysh038@naver.com>
2022-03-31 16:24:58 +0900

    js : navbar menu toggle + project UI tweak

--- main.js
+++ main.js
@@ -21,7 +21,14 @@ navbarMenu.addEventListener("click", (event) => {
     if (link == null) {
         return;
     }
+    navbarMenu.classList.remove("open");
     scrollIntoView(link);
+});
+
+//Navbar toggle button fro small screen
+const navbarToggleBtn = document.querySelector(".navbar__toggle-btn");
+navbarToggleBtn.addEventListener("click", () => {
+    navbarMenu.classList.toggle("open");
 });
 
 /*const menu_button = document.querySelector(".navbar__menu:nth-child(1)");
